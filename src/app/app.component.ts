27af1d9386b60350f8f42d519c005b27da7fea60
Matthew Feng <mattfeng@mit.edu>
2018-07-17 11:35:20 -0400

    Add autologin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,34 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthService } from '../services/auth.service';
 
 import { LandingPage } from '../pages/landing/landing';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 
 export class Padl {
-  rootPage: any = LandingPage;
+  rootPage: any;
 
   constructor(platform: Platform, statusBar: StatusBar,
     splashScreen: SplashScreen, private auth: AuthService) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.initializeApp();
+    });
+  }
+
+  initializeApp() {
+    this.auth.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.rootPage = TabsPage;
+      } else {
+        this.rootPage = LandingPage;
+      }
+    }, () => {
+      this.rootPage = LandingPage;
     });
   }
 }
