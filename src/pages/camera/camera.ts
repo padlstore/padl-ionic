07b5b888fe67bde65fc40f03de4f10ec2874671a
Nikhil Murthy <murthynikhil@live.com>
2018-07-17 19:16:47 -0400

    Added camera features
    Added camera features (taking photo, sizing to a square, displaying on page)

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { normalizeURL } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
     templateUrl: 'camera.html'
 })
 export class CameraPage {
+	public base64Image : string;
+	public photoTaken : boolean;
 
-    constructor() {
+	constructor(private camera: Camera, public platform: Platform) { 
+		this.photoTaken = false; // initially, do not display any photos as no photo was taken
+	}
+    
 
-    }
+	ionViewDidEnter(){
+
+		const options: CameraOptions = {
+			  quality: 100,
+			  destinationType: this.camera.DestinationType.FILE_URI,
+			  encodingType: this.camera.EncodingType.JPEG,
+			  mediaType: this.camera.MediaType.PICTURE,
+			  allowEdit: true
+			}
+
+		this.camera.getPicture(options).then((imageData) => {
+		 // imageData is either a base64 encoded string or a file URI
+		 // If it's base64 (DATA_URL):
+
+		 this.photoTaken = true;
+
+		if (this.platform.is('ios'))
+        	this.base64Image = normalizeURL(imageData);
+      	else
+        	this.base64Image = "data:image/jpeg;base64," + imageData;
+
+		}, (err) => {
+		 	console.debug("Unable to obtain picture: " + err, "app");
+      		console.log(err);
+		});
+	}	
+	
 }
